8122da62f2a3ae65e646e79e3797798d10466ae5
slqpez <santiago.lopez16@udea.edu.co>
2020-08-18 18:01:22 -0500

    Prototype showMsg

--- assets/js/interfaz.js
+++ assets/js/interfaz.js
@@ -2,11 +2,15 @@ function Interfaz() {}
 
 Interfaz.prototype.showMsg = function (msg, classMsg) {
   const form = document.querySelector("form");
-  const msgDiv = document.createElement("div");
-  msgDiv.classList.add(classMsg);
-  msgDiv.textContent = msg;
-  console.log("buena");
-  form.insertBefore(msgDiv, document.querySelector("#models"));
+  if (document.querySelector("form div") === null) {
+    const msgDiv = document.createElement("div");
+    msgDiv.classList.add(classMsg);
+    msgDiv.textContent = msg;
+    form.insertBefore(msgDiv, document.querySelector("#models"));
+    setTimeout(() => {
+      msgDiv.remove();
+    }, 2000);
+  }
 };
 
 const form = document.querySelector("form");
