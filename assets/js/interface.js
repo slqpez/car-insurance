14e097bdde3749ad43ad332f758c03e322d36d7d
slqpez <santiago.lopez16@udea.edu.co>
2020-08-19 11:29:44 -0500

    Done, need to do responsive.

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -8,9 +8,9 @@ function Interfaz() {
 
 Interfaz.prototype.showMsg = function (msg, classMsg) {
   const form = document.querySelector("form");
-  if (!document.querySelector("form div")) {
+  if (!document.querySelector(".to-remove")) {
     const msgDiv = document.createElement("div");
-    msgDiv.classList.add(classMsg);
+    msgDiv.classList.add(classMsg, "to-remove");
     msgDiv.textContent = msg;
     form.insertBefore(msgDiv, document.querySelector("#models"));
     setTimeout(() => {
@@ -46,32 +46,50 @@ Interfaz.prototype.getType = function () {
   return this.typeS;
 };
 
+Interfaz.prototype.createMsg = function (model, year, type, total) {
+  const quotDiv = document.createElement("div");
+  quotDiv.classList.add("quot-msg");
+  quotDiv.innerHTML = `<p>Modelo: ${model}</p>
+  <p>Año: ${year}</p>
+  <p>Tipo de seguro: ${type}</p>
+  <p>Valor total: $${total}</p>`;
+  form.insertBefore(quotDiv, document.querySelector(".btn"));
+};
+
 Interfaz.prototype.showQuot = function (model, year, type, total) {
   const form = document.querySelector("form");
 
   if (!document.querySelector(".quot-msg")) {
-    const quotDiv = document.createElement("div");
-    quotDiv.classList.add("quot-msg");
-    quotDiv.innerHTML = `<p>Modelo: ${model}</p>
-  <p>Año: ${year}</p>
-  <p>Tipo de seguro: ${type}</p>
-  <p>Valor total: ${total}</p>`;
-    form.insertBefore(quotDiv, document.querySelector(".btn"));
-    console.log("what");
+    setTimeout(() => {
+      this.createMsg(model, year, type, total);
+    }, 2000);
   } else {
+    const exist = document.querySelector(".quot-msg");
+    exist.remove();
+    setTimeout(() => {
+      this.createMsg(model, year, type, total);
+    }, 2000);
   }
+};
+
+Interfaz.prototype.showSpinner = function () {
+  const spinner = document.querySelector("iframe");
+  spinner.style.display = "block";
+  setTimeout(function () {
+    spinner.style.display = "none";
+  }, 2000);
 };
 
 Interfaz.prototype.generateQuot = function () {
   if (this.isComplete()) {
-    this.showMsg("Cotizando", "complete");
+    this.showSpinner();
     const model = this.getModel();
     const year = this.getYear();
     const typeS = this.getType();
     const quot = new Quot(model, year, typeS);
     const totalValue = quot.getQuot();
+    this.showMsg("Cotizando", "complete");
     this.showQuot(model, year, typeS, totalValue);
-    console.log(totalValue);
   } else {
     this.showMsg("Faltan datos.", "error");
   }
